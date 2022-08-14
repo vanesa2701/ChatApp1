56ab9287532501e9ff280d548202f69d76462081
vanesa2701 <v.kubazi@gmail.com>
2022-08-14 13:41:18 +0200

    initial commit

---
+++ Net/Server.cs
@@ -0,0 +1,79 @@
+﻿using chatclient.Net.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chatclient.Net
+{
+    internal class Server
+    {
+        TcpClient _client;
+        PacketBuilder _packetBuilder;
+       public PacketReader PacketReader;
+        public event Action connectedEvent;
+        public event Action userDisconnectEvent;
+        public event Action msgReceivedEvent;
+
+
+        public Server()
+        {
+            _client = new TcpClient();
+        }
+
+        public void ConnectToServer(string username)
+        {
+            if (!_client.Connected)
+            {
+                _client.Connect("127.0.0.1", 7891);
+                PacketReader = new PacketReader(_client.GetStream());
+                if (!string.IsNullOrEmpty(username))
+
+                {
+                    var connectPacket = new PacketBuilder();
+                    connectPacket.WriteOpCode(0);
+                    connectPacket.WriteString(username);
+                    _client.Client.Send(connectPacket.GetPacketBytes());
+                }
+                ReadPackets();
+            }
+        }
+
+        private void ReadPackets()
+        {
+            Task.Run(() => {
+            while (true)
+                {
+                    var opcode = PacketReader.ReadByte();
+                    switch(opcode){
+                        case 1:
+                            connectedEvent?.Invoke();
+                            break;
+                            case 5:
+                            msgReceivedEvent?.Invoke();
+                            break;
+                        case 10:
+                            userDisconnectEvent?.Invoke();
+                            break;
+
+
+
+
+                        default:
+                            Console.WriteLine("upsi tupsi");
+                            break;
+                    }
+                }
+                    });
+        }
+        public void SendMessageToServer(string message)
+        {
+            var messagePacket = new PacketBuilder();
+            messagePacket.WriteOpCode(5);
+            messagePacket.WriteMessage(message);
+            _client.Client.Send(messagePacket.GetPacketBytes());
+        }
+    }
+}
